591747e82a8050991740ae3d0332687782ac6dbe
Pranjalkoli26 <147383051+Pranjalkoli26@users.noreply.github.com>
2023-11-22 19:27:04 +0530

    Update UserServicesApplication.java

--- UserServicesApplication.java
+++ UserServicesApplication.java
@@ -1,17 +1,3 @@
-package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-@EnableJpaRepositories
-@EnableEurekaClient
-public class UserServicesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserServicesApplication.class, args);
-	}
-
-}
